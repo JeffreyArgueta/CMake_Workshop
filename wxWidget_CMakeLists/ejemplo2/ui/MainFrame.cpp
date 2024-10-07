3786c279cf56ba8959ad61a397fcf55323cdbff3
Fort <christopheralvarez503@gmail.com>
2024-10-06 21:51:33 -0600

    Corrección en el modulo ui del ejemplo 2 de wxWidgets

--- wxWidget_CMakeLists/ejemplo2/ui/MainFrame.cpp
+++ wxWidget_CMakeLists/ejemplo2/ui/MainFrame.cpp
@@ -18,7 +18,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title, 
     wxBoxSizer* contentSizer = new wxBoxSizer(wxVERTICAL);
 
     // Título centrado
-    wxStaticText* titleText = new wxStaticText(panel, wxID_ANY, wxT("Bienbenidos desde el modulo ui"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
+    wxStaticText* titleText = new wxStaticText(panel, wxID_ANY, wxT("Bienvenidos desde el modulo ui"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
     titleText->SetFont(wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD)); // Fuente más grande y negrita
     contentSizer->Add(titleText, 0, wxALL | wxALIGN_CENTER, 15);
 
