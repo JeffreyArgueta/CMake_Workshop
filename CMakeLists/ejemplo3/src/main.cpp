fba603e51ee4e4bf59bc5be2d45e7e3af8e0e0a5
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2024-10-06 19:14:02 -0600

    Directories added

---
+++ CMakeLists/ejemplo3/src/main.cpp
@@ -0,0 +1,6 @@
+#include "../include/hello.hpp"
+
+int main (int argc, char *argv[]) {
+    hello();
+    return 0;
+}
--- ejemplo2/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/hello.hpp"
-
-int main (int argc, char *argv[]) {
-    hello();
-    return 0;
-}
